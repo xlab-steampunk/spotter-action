9cb6b13674f5c5389a6d53306aad91b2b59405c8
Anže Luzar <anze.luzar@xlab.si>
2023-04-25 10:35:53 +0200

    Support setting and clearing custom polices (enterprise feature)
    Set custom OPA policies before scanning and clear them after if needed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,29 @@ display_level="$5"
 no_docs_url="$6"
 ansible_version="$7"
 profile="$8"
+custom_policies_path="$9"
+custom_policies_clear="${10}"
+
+# helper functions for building CLI commands
+buildSetPoliciesCommand() {
+  set_policies_command="spotter set-policies"
+
+  if [ -n "$project_id" ]; then
+    set_policies_command="${set_policies_command} --project-id ${project_id}"
+  fi
+
+  if [ -n "$custom_policies_path" ]; then
+    set_policies_command="${set_policies_command} $custom_policies_path"
+  fi
+}
+
+buildClearPoliciesCommand() {
+  clear_policies_command="spotter clear-policies"
+
+  if [ -n "$project_id" ]; then
+    clear_policies_command="${clear_policies_command} --project-id ${project_id}"
+  fi
+}
 
 buildScanCLICommand() {
   scan_command="spotter scan"
@@ -46,8 +69,18 @@ buildScanCLICommand() {
   fi
 }
 
-# construct the CLI command
+# construct CLI commands
+buildSetPoliciesCommand
 buildScanCLICommand
+buildClearPoliciesCommand
 
-# run the CLI command
-${scan_command}
+# run CLI commands
+if [ -n "$custom_policies_path" ]; then
+  ${set_policies_command}
+  ${scan_command}
+  if [ "$custom_policies_clear" = "true" ]; then
+    ${clear_policies_command}
+  fi
+else
+  ${scan_command}
+fi
